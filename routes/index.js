06da8838d32dd5c24a145a58e0a29d1625d17d11
Zephyr <zephyr.pellerin@gmail.com>
2013-08-24 11:36:30 -0700

    Return results inside of mongo toArray callback

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,23 @@
  * GET home page.
  */
 
+
+function deriveTiles() {
+  console.log("DERIVETILES");
+
+}
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  mongo_client = require('mongodb').MongoClient;
+  format = require('util').format; 
+  mongo_client.connect('mongodb://127.0.0.1:27017/linkshare', function(err, db) {
+    collection = db.collection('tiles');
+    hhhh = collection.find().toArray(function(err, results) {
+        console.log(results);
+        res.render('index', { title: 'Express', tiles: results });
+      }); 
+  });
+  
 };
+
+
