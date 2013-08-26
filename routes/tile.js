b8e0a5d2ba256dcdaddb14a4e9e7ecb79f67a202
Drew Winget <awinget@stanford.edu>
2013-08-26 01:55:09 -0700

    fix for clicking links

--- routes/tile.js
+++ routes/tile.js
@@ -4,38 +4,38 @@
  */
 
 exports.update = function(req, res){
-    mongo_client.connect(_mdb, function(err, db) {
-	var tileq = {'uid': req.body.uid,
-		     'pageURL': req.body.pageURL
-		    };
-	var collection = db.collection('tiles');	
-	collection.findOne(tileq, function(err, tile) {
-	    if (!err) {
-		if (tile) {
-		    collection.update(tileq, req.body, function(err, modified) {
-			res.send(modified);
-		    });
-		} else {
-		    collection.insert(req.body, function(err, records) {
-			res.send(records);
-		    });
-		}
-	    } else {
-		res.send(500);
-	    }
-	});
+  mongo_client.connect(_mdb, function(err, db) {
+    var tileq = {'uid': req.body.uid,
+      'pageURL': req.body.pageURL
+    };
+    var collection = db.collection('tiles');	
+    collection.findOne(tileq, function(err, tile) {
+      if (!err) {
+        if (tile) {
+          collection.update(tileq, req.body, function(err, modified) {
+            res.send(modified);
+          });
+        } else {
+          collection.insert(req.body, function(err, records) {
+            res.send(records);
+          });
+        }
+      } else {
+        res.send(500);
+      }
     });
+  });
 };
 
 exports.read = function(req, res){
-    mongo_client.connect(_mdb, function(err, db) {
-	var collection = db.collection('tiles');
-	collection.find().toArray(function(err, results) {
-	    if (!err) {
-		res.send(results);
-	    } else {
-		res.send(500);
-	    }
-	});
+  mongo_client.connect(_mdb, function(err, db) {
+    var collection = db.collection('tiles');
+    collection.find().toArray(function(err, results) {
+      if (!err) {
+        res.send(results);
+      } else {
+        res.send(500);
+      }
     });
+  });
 }
