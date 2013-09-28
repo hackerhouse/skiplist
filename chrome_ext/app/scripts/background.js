de4e826c607fbeacf3006f371107dd18496e5419
Zephyr <zephyr.pellerin@gmail.com>
2013-09-28 16:53:54 -0700

    Made some stylistic changes to chrome extension login

--- chrome_ext/app/scripts/background.js
+++ chrome_ext/app/scripts/background.js
@@ -1,5 +1,13 @@
-'use strict';
+chrome.browserAction.onClicked.addListener(function() {
+  // alert('gotclicked');
+  // Set a timeout to briefly display our image and then set it back
+  setTimeout(function() {
+    chrome.browserAction.setIcon({
+      path: 'images/icon-38.png'
+    });
+  }, 5000);
 
-chrome.runtime.onInstalled.addListener(function (details) {
-    console.log('previousVersion', details.previousVersion);
+  chrome.browserAction.setIcon({path:"images/icon-38-saved-state.png"});
 });
+
+chrome.browserAction.setIcon({path:"images/icon-38.png"});
