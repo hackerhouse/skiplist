b9309504994d3881681b06b9695f609f16419440
Zephyr <zephyr.pellerin@gmail.com>
2013-10-02 00:16:32 -0700

    Added in Gruntfile. Let me know if there are actions that still need to be ported over (besides uglify)

---
+++ Gruntfile.js
@@ -0,0 +1,98 @@
+// grunt
+var request = require('request');
+// shelljs shell
+var shell = require('shelljs');
+
+module.exports = function (grunt) {
+  var reloadPort = 35729, files;
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    develop: {
+      server: {
+        file: 'app.js'
+      }
+    },
+    // Watch Task
+    watch: {
+      options: {
+        nospawn: true,
+        livereload: reloadPort
+      },
+      js: {
+        files: [
+          'app.js',
+          'app/**/*.js',
+          'config/{,*/}*.js',
+          'public/styles/less/{,*/}*.less'
+        ],
+        tasks: ['develop', 'delayed-livereload']
+      },
+      less: {
+        development: {
+          options: {
+            paths: ['public/styles/less'],
+            yuicompress: true
+          }
+        },
+        files: ['public/styles/{,*/}*.less'],
+        tasks: ['less']
+      }
+    },
+    //
+    // Less
+    //
+    less: {
+      development: {
+        options: {
+          paths: ['public/styles/less'],
+          yuicompress: true
+        },
+        files: {
+          'public/styles/css/style.css': 'public/styles/less/style.less'
+        }
+      }
+    }
+  });
+
+  grunt.config.requires('watch.js.files');
+  files = grunt.config('watch.js.files');
+  files = grunt.file.expand(files);
+
+  grunt.registerTask('delayed-livereload', 'Live reload after the node server has restarted.', function () {
+    var done = this.async();
+    setTimeout(function () {
+      request.get('http://localhost:' + reloadPort + '/changed?files=' + files.join(','),  function(err, res) {
+          var reloaded = !err && res.statusCode === 200;
+          if (reloaded)
+            grunt.log.ok('Delayed live reload successful.');
+          else
+            grunt.log.error('Unable to make a delayed live reload.');
+          done(reloaded);
+        });
+    }, 500);
+  });
+
+  // this task should work, but doesn't anyway.
+  // this is what I get for going against the grunt task way.
+  grunt.registerTask('loadAuthors', 'give out cred', function() {
+    // grab our authors, cut out their contributions (showing that would be
+    // unclassy), sort and convert to JSON representable format.
+    var bower = grunt.file.readJSON('bower.json');
+    var pkg   = grunt.file.readJSON('package.json');
+    grunt.log.ok('Processing git log for authors');
+    var authors = shell.exec('git shortlog -sne | cut -f 1 --complement | sort | sed "s/^/\"/;s/$/\",/"');
+    var authorsObj = JSON.parse("[" + authors + "]");
+    bower.authors = pkg.contributors = authorsObj;
+    grunt.log.ok('Updating bower.json');
+    grunt.file.write(bower);
+    grunt.log.ok('Updating package.json');
+    grunt.file.write(pkg);
+  });
+
+  grunt.loadNpmTasks('grunt-develop');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.registerTask('default', ['develop', 'watch']);
+};
